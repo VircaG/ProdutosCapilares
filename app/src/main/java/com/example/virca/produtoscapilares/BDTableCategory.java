0f2e3646317955bb05f953179d5cfe6f689b6804
VircaG <vircagoncalves39@gmail.com>
2018-06-08 00:45:30 +0100

    CRUD BDTableCategory

--- app/src/main/java/com/example/virca/produtoscapilares/BDTableCategory.java
+++ app/src/main/java/com/example/virca/produtoscapilares/BDTableCategory.java
@@ -1,11 +1,14 @@
 package com.example.virca.produtoscapilares;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
 
 public class BDTableCategory implements BaseColumns {
     public static final String FIELD_NOME = "nome";
     public static final String _ID = "ID";
+    public static final String TABLE_NAME = "TABLE_NAME";
     private SQLiteDatabase db;
 
 
@@ -13,7 +16,47 @@ public class BDTableCategory implements BaseColumns {
         this.db = db;
     }
     public void create(){
-        db.execSQL("CREATE TABLE category(" + FIELD_NOME + " TEXT NOT NULL," + _ID + " INTEGER PRIMARY KEY AUTOINCREMENT)");
+        db.execSQL("CREATE TABLE category(" + FIELD_NOME + "" +
+                " TEXT NOT NULL," + _ID + " " +
+                "INTEGER PRIMARY KEY AUTOINCREMENT)"
+        );
     }
 
+    public static ContentValues getContentValues(Category category){
+        ContentValues values = new ContentValues();
+
+        values.put(_ID,category.getId());
+        values.put(FIELD_NOME,category.getNome());
+
+        return values;
+    }
+    public static Category getCurrentCategoryFromCursor(Cursor cursor){
+     final int posId = cursor.getColumnIndex(_ID) ;
+     final int postNome = cursor.getColumnIndex(FIELD_NOME);
+
+     Category category = new Category();
+
+     category.setId(cursor.getInt(posId));
+     category.setNome(cursor.getString(postNome));
+     return  category;
+
+    }
+
+    public long insert (ContentValues values){
+        return db.insert(TABLE_NAME,"null",values); }
+
+
+
+
+
+        public int delete(String whereClause,String[] whereArgs){
+        return db.insert(TABLE_NAME,whereClause,whereArgs);
+    }
+    public Cursor query(String [] columns ,String selection,
+                        String[] selectionArgs,String groupBy,String having,
+                        String orderBy){
+        return db.query(TABLE_NAME,columns,selection,selectionArgs,
+                groupBy,having,orderBy);
+
+    }
 }

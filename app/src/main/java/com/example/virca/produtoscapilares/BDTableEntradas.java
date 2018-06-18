450c1e09114437f2ede7830a772b4adf2e14369c
VircaG <vircagoncalves39@gmail.com>
2018-06-18 02:44:19 +0100

    Creat class entrada

--- app/src/main/java/com/example/virca/produtoscapilares/BDTableEntradas.java
+++ app/src/main/java/com/example/virca/produtoscapilares/BDTableEntradas.java
@@ -1,13 +1,18 @@
 package com.example.virca.produtoscapilares;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
 
 public class BDTableEntradas implements BaseColumns {
     public static final String FIELD_QUANTIDADE = "quantidade";
-    public static final String FIELD_DATA = "data";
+    public static final String FIELD_DATA = "date";
     public static final String FIELD_ID_PRODUTO = "id_produto";
     public static final String TABLE_NAME = "entrada";
+
+
+
 
     private SQLiteDatabase db;
 
@@ -22,18 +27,22 @@ public class BDTableEntradas implements BaseColumns {
                         FIELD_QUANTIDADE + " INTENGER," +
                         FIELD_DATA + " TEXT" +
                         ")"
-//                "CREATE TABLE " + TABLE_NAME + "(" +
-//                        _ID + " INTEGER PRIMARY KEY AUTOINCREMENT ," +
-//                        DATE + "TEXT NOT NULL," +
-//                        FIELD_QUANTIDADE_PRO + "INTENGER," +
-//                        FIELD_ID_PRODUTO + " INTEGER,"  +
-//
-//
-//                        "FOREIGN KEY ("+ FIELD_ID_PRODUTO +") REFERENCES " +
-//                        BDTableProduto.TABLE_NAME +
-//                                "("+BDTableProduto._ID_PRODUTO+ ")"
+
         );
 
     }
 
+
+    public static ContentValues getContentValues (Entrada entrada){
+        ContentValues values = new ContentValues();
+
+        values.put(_ID,entrada.getId());
+        values.put(FIELD_DATA,entrada.getDate());
+        values.put(FIELD_ID_PRODUTO,entrada.getId_produto());
+        values.put(FIELD_QUANTIDADE,entrada.getQuantidade());
+
+
+        return values;
+    }
+public static Entrada getCurrentCategoryFromCursor(Cursor cursor)
 }
---
+++ app/src/main/java/com/example/virca/produtoscapilares/Entrada.java
@@ -0,0 +1,42 @@
+package com.example.virca.produtoscapilares;
+
+import java.util.Date;
+
+public class Entrada {
+    private  int id;
+    private int quantidade;
+    private Date date;
+    private int id_produto;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getId_produto() {
+        return id_produto;
+    }
+
+    public void setId_produto(int id_produto) {
+        this.id_produto = id_produto;
+    }
+}

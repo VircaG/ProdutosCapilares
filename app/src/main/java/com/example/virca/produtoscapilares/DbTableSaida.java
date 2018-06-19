a9eed01abec073f2f944cf29352a3496af4f15c0
VircaG <vircagoncalves39@gmail.com>
2018-06-19 13:03:09 +0100

    Test for BDTableSaida

--- app/src/main/java/com/example/virca/produtoscapilares/DbTableSaida.java
+++ app/src/main/java/com/example/virca/produtoscapilares/DbTableSaida.java
@@ -1,5 +1,6 @@
 package com.example.virca.produtoscapilares;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
@@ -8,10 +9,11 @@ import java.util.Calendar;
 
 public class DbTableSaida implements BaseColumns {
     public static final String TABLE_NAME = "saida";
+
     public static final String FIELD_DATA = "data";
     public static final String FIELD_QUANTIDADE = "quantidade";
-
     public static final String FIELD_ID_PRODUTO = "id_produto";
+
     private SQLiteDatabase db;
 
     public DbTableSaida(SQLiteDatabase db){
@@ -30,9 +32,46 @@ public class DbTableSaida implements BaseColumns {
         );
     }
 
-    public static  Saida getCurrentSaidaFromCursor( Cursor cursor){
-   final int  posIdProduto = cursor.getColumnIndex(_ID);
-   final int posquantidade = cursor.getColumnIndex(pos);
+    public static ContentValues getContentValues( Saida saida){
+        ContentValues values = new ContentValues();
+
+        values.put(_ID,saida.getId());
+        values.put(FIELD_DATA,saida.getData());
+        values.put(FIELD_ID_PRODUTO,saida.getId_produto());
+        values.put(FIELD_QUANTIDADE,saida.getQuantidade());
+
+        return values;
     }
+
+    public static  Saida getCurrentSaidaFromCursor( Cursor cursor){
+       final int posId = cursor.getColumnIndex(_ID);
+       final int posQuantidade = cursor.getColumnIndex(FIELD_QUANTIDADE);
+       final int posIdProduto = cursor.getColumnIndex(FIELD_ID_PRODUTO);
+       final int posData = cursor.getColumnIndex(FIELD_DATA);
+
+       Saida saida = new Saida();
+
+       saida.setId(cursor.getInt(posId));
+       saida.getData(cursor.getString(posData));
+       saida.getId_produto(cursor.getInt(posIdProduto));
+       saida.getQuantidade(cursor.getInt(posQuantidade));
+
+       return saida;
+
+    }
+    public  long inserte(ContentValues values ){
+        return db.insert(TABLE_NAME,null,values );
+    }
+    public int update(ContentValues values,String whereClause, String[] whereArgs){
+        return db.update(TABLE_NAME,values,whereClause,whereArgs);
+    }
+    public int delete(String whereClause ,String[] whereArgs){
+        return db.delete(TABLE_NAME,whereClause,whereArgs);
+    }
+    public Cursor query(String[] columns,String selection,String[] selectionArgs,
+                        String groupBy,String having,String orderBy){
+        return db.query(TABLE_NAME,columns,selection,selectionArgs,groupBy,having,orderBy);
+    }
+
 
 }
--- app/src/main/java/com/example/virca/produtoscapilares/Saida.java
+++ app/src/main/java/com/example/virca/produtoscapilares/Saida.java
@@ -1,6 +1,7 @@
 package com.example.virca.produtoscapilares;
 
 public class Saida {
+    private int id;
     private String data;
     private int quantidade;
     private int id_produto;
@@ -27,5 +28,13 @@ public class Saida {
 
     public void setId_produto(int id_produto) {
         this.id_produto = id_produto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }

359389af476326f4ea174b6ecc5e423a721c7126
VircaG <vircagoncalves39@gmail.com>
2018-06-20 22:44:17 +0100

    Adição Produtos Capilares Content Proviber

--- app/src/main/java/com/example/virca/produtoscapilares/ProdutosCapilaresContentProvider.java
+++ app/src/main/java/com/example/virca/produtoscapilares/ProdutosCapilaresContentProvider.java
@@ -1,7 +1,11 @@
 package com.example.virca.produtoscapilares;
 
 import android.content.ContentProvider;
+import android.content.ContentValues;
 import android.content.UriMatcher;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 public class ProdutosCapilaresContentProvider extends ContentProvider {
 
@@ -18,6 +22,22 @@ public class ProdutosCapilaresContentProvider extends ContentProvider {
         bdProdutoCapilaresOpenHelper  = new BDProdutosCapilaresOpenHelper(getContext());
 
         return true;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    private void notifyChanges (@NonNull Uri uri){
+        getContext().getContentResolver().notifyChange(uri,null);
     }
 
 }

2377f34d7fa3bca3daeee15f7bea583636b94de3
VircaG <vircagoncalves39@gmail.com>
2018-06-24 06:31:08 +0100

    Adição Produtos Capilares Base de dados seed

--- app/src/main/java/com/example/virca/produtoscapilares/BDProdutosCapilaresOpenHelper.java
+++ app/src/main/java/com/example/virca/produtoscapilares/BDProdutosCapilaresOpenHelper.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class BDProdutosCapilaresOpenHelper extends SQLiteOpenHelper {
+    private static final boolean PRODUCTION = false;
 
     public static final String DATABASE_NAME = "ProdutosCapilares.db";
     public static final int DATABASE_VERSION = 1;
@@ -16,6 +17,7 @@ public class BDProdutosCapilaresOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
         BDTableCategory bdTableCategory = new BDTableCategory(db);
         bdTableCategory.create();
 
@@ -28,7 +30,58 @@ public class BDProdutosCapilaresOpenHelper extends SQLiteOpenHelper {
         BDTableSaida bdTableSaida = new BDTableSaida(db);
         bdTableSaida.create();
 
+        if (!PRODUCTION){
+            seed(db);
+        }
+
     }
+    private  void seed(SQLiteDatabase db){
+       BDTableCategory bdTableCategory = new BDTableCategory(db);
+
+        Category category = new Category();
+        category.setNome("Shampoo");
+        int idCategoryShampoo = (int) bdTableCategory.insert(BDTableCategory.getContentValues(category));
+
+
+        category = new Category();
+        category.setNome("Amaciador");
+        int idCategoryAmaciador = (int) bdTableCategory.insert(BDTableCategory.getContentValues(category));
+
+
+        category = new Category();
+        category.setNome("Tonico");
+        int  idCategoryTonico = (int) bdTableCategory.insert(BDTableCategory.getContentValues(category));
+
+        BDTableProduto bdTableProduto = new BDTableProduto(db);
+
+        ProdutosCapilares produtosCapilares = new ProdutosCapilares();
+        produtosCapilares.setNome("Argan Oil of Marocco");
+        produtosCapilares.setIdCategory(idCategoryShampoo);
+        produtosCapilares.setQuantidade(5);
+        produtosCapilares.setDate(12-06-18); //perguntar o professor
+        bdTableProduto.insert(BDTableProduto.getContentValues(produtosCapilares));
+
+        produtosCapilares = new ProdutosCapilares();
+        produtosCapilares.setNome("Pro-Vitaminas Bomba Café");
+        produtosCapilares.setIdCategory(idCategoryAmaciador);
+        produtosCapilares.setQuantidade(5);
+        produtosCapilares.setDate(12-06-18); //perguntar professor
+        bdTableProduto.insert(BDTableProduto.getContentValues(produtosCapilares));
+
+
+        produtosCapilares = new ProdutosCapilares();
+        produtosCapilares.setNome("Puro Tónico Alho");
+        produtosCapilares.setIdCategory(idCategoryTonico);
+        produtosCapilares.setQuantidade(2);
+        produtosCapilares.setDate(12-06-18); //perguntar o professor
+        bdTableProduto.insert(BDTableProduto.getContentValues(produtosCapilares));
+
+
+
+
+
+    }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
